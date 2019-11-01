d7f483997b2f8df29290aae964d5581cfc735465
Julie Christie <julie.christie@temple.edu>
2019-11-01 12:29:00 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+// Import Bootstrap CSS and JS
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./custom.css";
+
+// Import components for rendering based on question/language
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
